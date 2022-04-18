6047477240c99b05f6974a976c5a2a3c65348c93
Quinn Pollock <contact@quinnpollock.net>
2022-04-17 20:09:09 -0700

    make needed changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
+import {
+	defineConfig
+} from 'astro/config'
 // Full Astro Configuration API Documentation:
 // https://docs.astro.build/reference/configuration-reference
 
 // @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
+export default defineConfig({
 	// Comment out "renderers: []" to enable Astro's default component support.
 	integrations: [],
+	site: 'https://holopollock.net'
 });
