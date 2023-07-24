fabf5fbb860486319d401305226cb1b9fbe35f45
Quinn Pollock <contact@quinnpollock.net>
2023-07-24 14:01:39 -0700

    Update globalVars.ts

--- src/globalVars.ts
+++ src/globalVars.ts
@@ -11,7 +11,7 @@ export const PersonInfo: Person = {
 
 export const Socials: SocialsType = {
     twitterLink: "https://twitter.com/holopollock",
-    instagramLink: "https://www.instagram.com/quandace/", 
+    instagramLink: "https://www.instagram.com/holopollock/", 
     tiktokLink: "https://www.tiktok.com/@holopollock",
     spotifyLink: "https://open.spotify.com/user/7vmoh3e6cw563y4nn8vdp2oog?si=7f0240558cc14bb6",
     mastadonLink: "https://social.audiovalentine.com/@holopollock",
